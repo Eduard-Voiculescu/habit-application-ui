706c8a0caad8f8d526523f9cf0a54fc5f5a53831
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2021-06-27 19:04:31 -0400

    Adding front end code

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="w-100">
+      <div class="row">
+        <div class="col">
+          <ProfileSection />
+        </div>
+        <div class="col">
+          <HabitList />
+        </div>
+        <div class="col-8">
+          <DescriptionHabit />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileSection from "@/components/ProfileSection";
+import HabitList from "@/components/HabitList";
+import DescriptionHabit from "@/components/DescriptionHabit";
+
+export default {
+  name: 'App',
+  components: {
+    ProfileSection,
+    HabitList,
+    DescriptionHabit
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
