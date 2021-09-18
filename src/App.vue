da9ff3059cfe1e871ded6d0eaafa938ce6288dee
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2021-09-18 18:16:16 -0400

    Removing profile section

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@
     <div class="w-75 margin-100">
       <div class="row">
         <div class="col">
-          <ProfileSection />
-        </div>
-        <div class="col">
           <HabitList />
         </div>
         <div class="col-8">
@@ -18,14 +15,12 @@
 
 <script>
 import '@/assets/styles/app.css';
-import ProfileSection from "@/components/ProfileSection";
 import HabitList from "@/components/HabitList";
 import DescriptionHabit from "@/components/DescriptionHabit";
 
 export default {
   name: 'App',
   components: {
-    ProfileSection,
     HabitList,
     DescriptionHabit
   }
--- src/main.js
+++ src/main.js
@@ -2,34 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import axios from "axios";
-import router from './router/router'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-// Import the Auth0 configuration
-import { domain, clientId } from "./auth/auth_config.json";
-// Import the plugin here
-import { Auth0Plugin } from "./auth";
-
-Vue.use(Auth0Plugin, {
-  domain,
-  clientId,
-  onRedirectCallback: appState => {
-    router.push(
-        appState && appState.targetUrl
-            ? appState.targetUrl
-            : window.location.pathname
-    );
-  }
-});
-
+Vue.config.productionTip = false
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-Vue.config.productionTip = false
-
 Vue.prototype.$http = axios;
 
 new Vue({
